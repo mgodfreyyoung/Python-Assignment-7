5f75ca8fa5348cb68acb41b6949d2eeb3a06e587
Michael G Young <mgodfreyyoung@gmail.com>
2012-04-11 23:50:51 -0600

    after getting re to work with http
    Signed-off-by: Michael G Young <mgodfreyyoung@gmail.com>

--- NeedleStack.py
+++ NeedleStack.py
@@ -1,8 +1,21 @@
-from urllib import *
+testString = '''http://freshsources.com/page5.html">Page 5</a>. And here is a link back to <a href   = "http://freshsources.com/page3.html">Page 3</a>.'''
+
+import urllib
+import re
 def get_page(url):
     try:
         return urllib.urlopen(url).read()
     except:
         return ""
     
-print get_page('http://freshsources.com/page5.html')
+
+def get_all_links(urlString):
+    urlSet = {}
+    urlSet = set(re.findall(r'href\s*=\s*[\'"]?(\http://[^\'">]+)', urlString)) 
+    return urlSet
+
+print get_page('http://freshsources.com/page5.html')   
+urlString = get_page('http://freshsources.com/page5.html')
+
+print get_all_links(urlString)
+
